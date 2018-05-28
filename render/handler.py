e249c095a9a0682cb743999b382d44dd19d349f4
Alex Ellis <alexellis2@gmail.com>
2018-05-28 16:14:52 -0700

    Enable of-cloud usage with optional pretty route
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- render/handler.py
+++ render/handler.py
@@ -1,8 +1,10 @@
+import os
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
     with open("function/views/home.html") as f:
-        return f.read()
+        return f.read().replace("URL", os.getenv("assets_url", "/function/assets"))
 
